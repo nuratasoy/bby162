3f69a46427c87a9d9053377f9d34233d59374628
nuratasoy <36641963+nuratasoy@users.noreply.github.com>
2018-02-26 14:28:18 +0300

    Delete uygulama01.py

--- uygulama01.py
+++
@@ -1,3 +0,0 @@
-metin="Aristoteles ile öğretmeni Platon'un bilgi anlayışları çok fazla farklılık göstermez. Aristoteles'e göre de bilgi değişmez olanın bilgisidir.. "
-print(len(metin)) #==142
-print(str(metin[0:71]))
