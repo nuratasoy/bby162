5173f83ede98782bafc4f5d76f521a5df30a2c61
nuratasoy <36641963+nuratasoy@users.noreply.github.com>
2018-02-26 14:30:27 +0300

    Add files via upload

---
+++ uygulama01.py
@@ -0,0 +1,6 @@
+metin=input("metni girin:")
+metinyarisi=metin[: int(len(metin)/2)]
+print(metinyarisi)
+
+metin="Aristoteles ile öğretmeni Platon'un bilgi anlayışları çok fazla farklılık göstermez. Aristoteles'e göre de bilgi değişmez olanın bilgisidir.. "
+
